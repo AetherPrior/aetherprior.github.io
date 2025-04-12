4ca90cf092bf6f469c7b4cae01235a201aacda7d
AetherPrior <AetherPrior@users.noreply.github.com>
2025-04-12 16:53:11 +0000

    deploy: e003ee3266f108dfecfa3c5cc4a53bb7eb26b4b1

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-node export_to_latex.js
-pdflatex --output-directory=assets assets/Abhinav_Rao_CV.tex
-rm assets/*.aux assets/*.log assets/*.out
-sleep 1
-git add .
-git commit
-git push origin master
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo pacman -S texlive-basic texlive-bin texlive-core texlive-latexextra texlive-fontsextra texlive-bibtexextra texlive-formatsextra 
-sudo pacman -S nvm
-nvm install node
-npm init -y
-npm install jsdom canvas
